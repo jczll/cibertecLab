b57d0463c491a10a6f5c07545d00f8a7bfba3764
jczll <jczll@msn.com>
2018-10-17 21:22:40 -0500

    nuevo proyecto

---
+++ wcf/SolutionWCF/Chinook.DataAcces.Repository.UnitTest/InvoiceRespositoryTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using Chinook.DataAcces.Interface;
+using Chinook.Entities.Base;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chinook.DataAcces.Repository.UnitTest
+{
+    [TestClass]
+    public class InvoiceRespositoryTest
+    {
+        private readonly DbContext dbcontext;
+        private readonly IInvoiceRepository invoiceRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public InvoiceRespositoryTest()
+        {
+            this.dbcontext = new Model1();
+            invoiceRepository = new InvoiceRepository(dbcontext);
+            unitOfWork = new UnitOfWork(dbcontext);
+        }
+        [TestMethod]
+        public void BuscarCustomerAddres()
+        {
+            var listado = unitOfWork.InvoiceRepository.GetCustosmerId_Invoice(1);
+            Assert.IsTrue(listado.Count() > 0, "Ok");
+        }
+        [TestMethod]
+        public void Add()
+        {
+            var invoice = new Invoice();
+            invoice.CustomerId = 1;
+            invoice.InvoiceDate = DateTime.Now;
+            invoice.BillingAddress = "Remigio Silva 510";
+            invoice.BillingCity = "Jesus Maria";
+            invoice.BillingState = "Lima";
+            invoice.BillingCountry = "Peru";
+            invoice.BillingPostalCode = "Lima 12";
+            //Agregando Items 
+            invoice.InvoiceLine.Add(
+                new InvoiceLine()
+                {
+                    TrackId = 1,
+                    Quantity = 2,
+                    UnitPrice = 10
+                }
+            );
+            invoice.InvoiceLine.Add(
+                new InvoiceLine()
+                {
+                    TrackId = 1,
+                    Quantity = 2,
+                    UnitPrice = 10
+                }
+            );
+            //Sumando Totales 
+            invoice.Total = invoice.InvoiceLine.Sum(item => item.UnitPrice * item.Quantity);
+
+            unitOfWork.InvoiceRepository.Add(invoice);
+            unitOfWork.Complete();
+            unitOfWork.Dispose();
+            Assert.IsTrue(invoice.InvoiceId > 0, "ok");
+        }
+    }
+}
---
+++ wcf/SolutionWCF/Chinook.DataAcces.Repository/ArtistRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chinook.DataAcces.Interface;
+using Chinook.Entities.Base;
+
+namespace Chinook.DataAcces.Repository
+{
+    public class ArtistRepository : GenericRepository<Artist>, IArtistRepository
+    {
+        public ArtistRepository(DbContext pContext) : base(pContext)
+        {
+
+        }
+        public IEnumerable<Artist> GetListByName(string name)
+        {
+            return ((Model1)context).Artist.Where(item => item.Name.Contains(name)).ToList();
+        }
+
+        public IEnumerable<Artist> GetListByNameStore(string name)
+        {
+            return context.Database.SqlQuery<Artist>("usp_GetlistByName @Name", new SqlParameter("@name", name)).ToList();
+        }
+    }
+}
